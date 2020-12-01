a4b3c315d9726bf276bba93d199b9a3e47a69e48
Chuns <i@chuns.app>
2020-12-01 23:01:54 +0800

    feat: add navigation

--- .umirc.ts
+++ .umirc.ts
@@ -5,4 +5,9 @@ export default defineConfig({
   mode: 'site',
   favicon: 'https://avatars2.githubusercontent.com/u/73589642?s=200',
   logo: 'https://avatars2.githubusercontent.com/u/73589642?s=200',
+  navs: [
+    { title: '文档', path: '/docs' },
+    { title: '博客', path: '/blog' },
+    { title: 'GitHub', path: 'https://github.com/reonejs/reone' },
+  ],
 });
